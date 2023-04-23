ac4f9a103cad1c27a2c4b9a8cffe2947358662ef
Prakhar <gprakhar11@gmail.com>
2023-04-24 05:29:49 +0530

    streamlit-app-first-commit

---
+++ model_s/amnet.py
@@ -0,0 +1,114 @@
+from keras.models import Model
+from keras.layers import add, Activation,UpSampling2D,multiply,BatchNormalization
+from keras.layers import Input, Conv2D, MaxPooling2D, concatenate, Conv2DTranspose, Dropout
+import keras.backend as K
+
+def attention_block(x,gating,intershape):
+    shape_x = K.int_shape(x)
+    shape_g = K.int_shape(gating)
+
+    theta_x = Conv2D(intershape,(1,1),strides = (2,2),padding="same")(x)
+    shape_theta_x = K.int_shape(theta_x)
+
+    phi_g = Conv2D(intershape,(1,1),padding="same")(gating)
+
+    concat_xg = add([phi_g,theta_x])
+    act_xg = Activation('relu')(concat_xg)
+    psi = Conv2D(1,(1,1),padding="same")(act_xg)
+    sigmoid_xg = Activation('sigmoid')(psi)
+    shape_sigmoid = K.int_shape(sigmoid_xg)
+
+    upsample_psi = UpSampling2D(size = (shape_x[1]//shape_sigmoid[1],shape_x[2]//shape_sigmoid[2]))(sigmoid_xg)
+    y = multiply([upsample_psi,x])
+    result = Conv2D(shape_x[3],(1,1),padding="same")(y)
+    result_bn = BatchNormalization()(result)
+    return result_bn
+
+
+def multi_unet_model(n_classes=4, IMG_HEIGHT=256, IMG_WIDTH=256, IMG_CHANNELS=1):
+#Build the model
+    inputs = Input((IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS))
+    s = inputs
+
+    #Encoder
+    c1 = Conv2D(16, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(s)
+    d2 = MaxPooling2D((2, 2))(c1)
+    d3 = MaxPooling2D((2, 2))(d2)
+    d4 = MaxPooling2D((2, 2))(d3)
+    d5 = MaxPooling2D((2, 2))(d4)
+    c1 = Dropout(0.1)(c1)
+    c1 = Conv2D(16, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c1)
+    p1 = MaxPooling2D((2, 2))(c1)
+    p1 = concatenate([p1, d2])
+    
+    c2 = Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p1)
+    c2 = Dropout(0.1)(c2)
+    c2 = Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c2)
+    p2 = MaxPooling2D((2, 2))(c2)
+    p2 = concatenate([p2, d3])
+     
+    c3 = Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p2)
+    c3 = Dropout(0.2)(c3)
+    c3 = Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c3)
+    p3 = MaxPooling2D((2, 2))(c3)
+    p3 = concatenate([p3, d4])
+     
+    c4 = Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p3)
+    c4 = Dropout(0.2)(c4)
+    c4 = Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c4)
+    p4 = MaxPooling2D((2, 2))(c4)
+    p4 = concatenate([p4, d5])
+     
+    c5 = Conv2D(256, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p4)
+    c5 = Dropout(0.3)(c5)
+    c5 = Conv2D(256, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c5)
+    
+    #Decoder
+    e5 = c5
+    e6 = Conv2DTranspose(128, (2, 2), strides=(2, 2), padding='same')(e5)
+
+    u6 = Conv2DTranspose(128, (2, 2), strides=(2, 2), padding='same')(c5)
+    c4 = attention_block(c4,c5,128)
+    u6 = concatenate([u6, c4])
+    c6 = Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u6)
+    c6 = Dropout(0.2)(c6)
+    c6 = Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c6)
+    e7 = Conv2DTranspose(128, (2, 2), strides=(2, 2), padding='same')(concatenate([e6,c6]))
+
+    # e4 = c4
+    # e7 = Conv2DTranspose(128, (2, 2), strides=(2, 2), padding='same')(e4)
+
+    u7 = Conv2DTranspose(64, (2, 2), strides=(2, 2), padding='same')(c6)
+    c3 = attention_block(c3,c6,64)
+    u7 = concatenate([u7, c3])
+    c7 = Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u7)
+    c7 = Dropout(0.2)(c7)
+    c7 = Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c7)
+    e8 = Conv2DTranspose(128, (2, 2), strides=(2, 2), padding='same')(concatenate([e7,c7]))
+     
+    u8 = Conv2DTranspose(32, (2, 2), strides=(2, 2), padding='same')(c7)
+    c2 = attention_block(c2,c7,32)
+    u8 = concatenate([u8, c2])
+    c8 = Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u8)
+    c8 = Dropout(0.1)(c8)
+    c8 = Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c8)
+    e9 = Conv2DTranspose(128, (2, 2), strides=(2, 2), padding='same')(concatenate([e8,c8]))
+     
+    u9 = Conv2DTranspose(16, (2, 2), strides=(2, 2), padding='same')(c8)
+    c1 = attention_block(c1,c8,16)
+    u9 = concatenate([u9, c1], axis=3)
+    c9 = Conv2D(16, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u9)
+    c9 = Dropout(0.1)(c9)
+    c9 = Conv2D(16, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c9)
+    e10 = concatenate([e9,c9])
+     
+    outputs = Conv2D(n_classes, (1, 1), activation='softmax')(e10)
+     
+    model = Model(inputs=[inputs], outputs=[outputs])
+    
+    #NOTE: Compile the model in the main program to make it easy to test with various loss functions
+    #model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+    
+    #model.summary()
+    
+    return model
---
+++ streamlitapp.py
@@ -0,0 +1,150 @@
+import streamlit as st
+import numpy as np
+import nibabel as nib
+import os
+import cv2
+import tensorflow as tf
+
+import os
+os.environ["SM_FRAMEWORK"] = "tf.keras"
+import segmentation_models as sm
+
+from model_s.unet import multi_unet_model as unet
+from model_s.mnet import multi_unet_model as mnet
+from model_s.amnet import multi_unet_model as attention_mnet
+from model_s.aunet import multi_unet_model as attention_unet
+
+
+model_name = {
+        'U-Net': [unet, '/home/vaibhav/U-Net/App/weights/unet.h5'], 
+        'M-Net': [mnet, '/home/vaibhav/U-Net/App/weights/mnet.h5'], 
+        'Attention U-Net': [attention_unet, '/home/vaibhav/U-Net/App/weights/aunet.h5'], 
+        'Attention M-Net': [attention_mnet, '/home/vaibhav/U-Net/App/weights/amnet.h5'],
+    }
+
+st.set_page_config(layout="wide", page_icon=':heart:', page_title='MIS')
+
+st.title('MRI Image Segmentor')
+
+cot1 = st.container()
+cot3 = st.container()
+cot2 = st.container()
+cot4 = st.container()
+
+
+cot1_col1, cot1_col2 = cot1.columns(2, gap='large')
+cot2_col1, cot2_col2 = cot2.columns(2, gap='large')
+
+#if st.button('Upload your .nii.gz file'):
+
+def file_change():
+    st.session_state['run'] = False
+
+uploaded_file = cot1_col1.file_uploader("Upload your .nii.gz file", on_change=file_change)
+
+if uploaded_file is None:
+    cot1_col1.write("Please upload a file")
+else:
+    with open(os.path.join("tmp.nii.gz"),"wb") as f:
+        f.write(uploaded_file.getbuffer())
+
+    img = nib.load("tmp.nii.gz")
+    img = np.array(img.dataobj)/255.0
+
+    cot2_col1.subheader('Input Image')
+    st.session_state['slice'] = cot3.slider('Slice Number?', 0, img.shape[2]-1, 0)
+
+    img_res = []
+    for i in range(img.shape[2]):
+        img_res.append(cv2.resize(img[:, :, i], dsize=(256, 256), interpolation=cv2.INTER_CUBIC))
+
+    cot2_col1.image(img_res[st.session_state['slice']], clamp=True, caption='MRI', use_column_width='always')
+
+    img_res_np = np.asarray(img_res)
+    img_res = np.expand_dims(img_res_np, -1)  
+
+    with cot1_col2:
+        if 'run' not in st.session_state or st.session_state['run']==False:
+            form = st.empty()
+            with form.form("form"):
+                model_choice = st.radio(
+                    "Choose the Segmentation Model:",
+                    ('U-Net', 'M-Net', 'Attention U-Net', 'Attention M-Net'))
+                
+                submitted = st.form_submit_button("Run")
+                if submitted:
+                    st.session_state['run'] = True
+                    st.session_state['model_choice'] = model_choice
+                    model = model_name[model_choice][0](4, 256, 256, 1)
+                    model.load_weights(model_name[model_choice][1])
+                    model.compile(optimizer='adam', loss=sm.losses.CategoricalFocalLoss(), metrics=['accuracy'])
+                    
+                    pred = model.predict(img_res)
+                    pred_images = np.argmax(pred, axis=3)
+
+                    # layers = [layer.name for layer in model.layers[1:] if 'conv' in layer.name and 'conv2d_transpose' not in layer.name]
+                    # layers = [layer for i, layer in enumerate(layers) if i%2==1]
+                    
+                    successive_outputs = [layer.output for layer in model.layers[1:] if 'conv' in layer.name and 'conv2d_transpose' not in layer.name]
+                    successive_outputs = [layer for i, layer in enumerate(successive_outputs) if i%2==1]
+                    
+                    visualization_model = tf.keras.models.Model(inputs = model.input, outputs = successive_outputs)
+
+                    st.session_state['fmaps'] = visualization_model.predict(img_res)
+                    # st.session_state['layers'] = layers
+
+                    rgb_images = []
+
+                    for i in range(img_res_np.shape[0]):
+                        rgb = cv2.cvtColor(img_res_np[i],cv2.COLOR_GRAY2RGB)
+                        for j in range(rgb.shape[0]):
+                            for k in range(rgb.shape[1]):
+                                # ( (1-p)R1 + p*R2, (1-p)*G1 + p*G2, (1-p)*B1 + p*B2 )
+                                pixel = pred_images[i][j][k]
+                                opacity = 0.01
+                                if pixel!=0:
+                                    rgb[j][k][pixel-1] = opacity*255.0 + (1-opacity)*rgb[j][k][pixel-1]
+                        rgb_images.append(rgb)
+
+                    rgb_images = np.asarray(rgb_images)
+                    
+                    st.session_state['pred'] = rgb_images
+                    form.empty()
+
+        if 'run' in st.session_state and st.session_state['run']:
+            st.subheader('Current Model: ' + st.session_state['model_choice'])
+            button = st.empty()
+            if button.button('Choose Another Model'):
+                st.session_state['run'] = False
+                button.empty()
+                st.experimental_rerun()
+
+    with cot2_col2:
+        if 'run' in st.session_state and st.session_state['run']:
+            st.subheader('Segmented Image')
+            st.image(st.session_state['pred'][st.session_state['slice']], caption='Segmented Image', use_column_width='always', channels='RGB', clamp=True)
+
+    if 'run' in st.session_state and st.session_state['run']:
+        # st.write(st.session_state['layers'])
+        if cot4.button('Show Feature Maps'):
+            tabs = []
+
+            for i in range(len(st.session_state['fmaps'])):
+                tabs.append(f'Layer{i}')
+            
+            tabs = st.tabs(tabs)
+
+            for i in range(len(st.session_state['fmaps'])):
+                fmap = np.asarray(st.session_state['fmaps'][i])
+                images = []
+                for j in range(fmap.shape[3]):
+                    images.append(fmap[st.session_state['slice'],:,:,j])
+                
+                tabs[i].write(len(images))
+                tabs[i].image(images, clamp=True, width=128)
+
+
+
+
+
+
